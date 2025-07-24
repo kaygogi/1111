b399839cd98472094679bc7208fec0ce8911dee9
Kayle <kayle.nielsen@gmail.com>
2025-07-24 17:21:45 -0500

    info

---
+++ info.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# 1. A greeting.
+echo "Welcome to your Linux System"
+echo
+
+# 2. The time and date.
+echo "Today is:"
+date
+echo
+
+# 3. The system’s uptime.
+echo "Uptime:"
+upt=$( uptime \
+     | cut -d',' -f1-2 \
+     | sed 's/^[[:space:]]*//' )
+echo "    $upt"
+echo
+
+# 4. The operating system you are running.
+echo "Operating System:"
+os=$( hostnamectl \
+     | grep 'Operating System:' \
+     | cut -d ':' -f2- \
+     | sed 's/^[[:space:]]*//' )
+echo "    $os"
+echo
+
+# 5. The kernel version.
+echo "Running Kernel version:"
+ker=$( hostnamectl \
+      | grep '^Kernel:' \
+      | cut -d ':' -f2- \
+      | sed 's/^[[:space:]]*//' )
+echo "    $ker"
